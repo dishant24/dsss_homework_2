3f2421980c6d01ca6bd622f09681650beea872a6
Dishant Sutariya <54105645+dishant24@users.noreply.github.com>
2023-11-13 16:41:37 +0100

    Update setup.py
    change the author email

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import find_packages
 setup( name="dsss_homework_2",
       version="0.1", 
       author="Dishant", 
-      author email-"dishantkumar.sutariya@fau.de",
+      author email-"dishantsutariya24@gmail.com",
       packages-find packages(). 
       install requires=["numpy", "turtles"])
