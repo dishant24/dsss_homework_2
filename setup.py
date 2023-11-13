08a2fcab6045ca070b38d4521dceacce661a2110
Dishant Sutariya <54105645+dishant24@users.noreply.github.com>
2023-11-13 16:36:16 +0100

    Create setup.py
    Add setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup( name="dsss_homework_2",
+      version="0.1", 
+      author="Dishant", 
+      author email-"dishantkumar.sutariya@fau.de",
+      packages-find packages(). 
+      install requires=["numpy", "turtles"])
